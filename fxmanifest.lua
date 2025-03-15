f52a1a2c9e91b3c20f7596c50333299b1c95aded
Hope x <zaydmaachou@gmail.com>
2025-03-15 22:30:51 +0000

    SetUp

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+games { "gta5" }
+fx_version "cerulean"
+lua54 "yes"
+
+description "React Boilerplate For FiveM NUI"
+author "@maachrpp"
+version "0.1.0 (Beta)"
+
+
+
+ui_page "web/build/index.html"
+
+client_script "client/**/*"
+
+files {
+    "web/build/index.html",
+    "web/build/**/*"
+}
